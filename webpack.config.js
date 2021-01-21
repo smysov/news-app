ba354bc182436b9510afa9129a7f99f114c73207
Sergey Mysov <s.mysov@list.ru>
2021-01-22 00:49:26 +0400

    feat: added form for search news

---
+++ webpack.config.js
@@ -0,0 +1,136 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+	const config = {
+		splitChunks: {
+			chunks: 'all',
+		},
+	};
+
+	if (isProd) {
+		config.minimizer = [new OptimizeCssAssetWebpackPlugin(), new TerserWebpackPlugin()];
+	}
+	return config;
+};
+
+const plugins = () => {
+	const base = [
+		new HTMLWebpackPlugin({
+			filename: 'index.html',
+			template: './index.html',
+			minify: {
+				collapseWhitespace: isProd,
+			},
+		}),
+		// new CopyWebpackPlugin({
+		// 	patterns: [
+		// 		{
+		// 			from: path.resolve(__dirname, 'src/fonts/'),
+		// 			to: path.resolve(__dirname, 'dist/fonts'),
+		// 		},
+		// 	],
+		// }),
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: filename('css'),
+		}),
+	];
+
+	if (isProd) {
+		base.push(new BundleAnalyzerPlugin());
+	}
+
+	return base;
+};
+
+const filename = ext => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	mode: 'development',
+	entry: {
+		main: ['@babel/polyfill', './index.js'],
+	},
+	output: {
+		filename: filename('js'),
+		path: path.resolve(__dirname, 'dist/'),
+	},
+	resolve: {
+		extensions: ['.js', '.json', '.png', '.csv'],
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	optimization: optimization(),
+	devServer: {
+		port: 4200,
+		open: true,
+	},
+	devtool: isDev ? 'source-map' : false,
+	plugins: plugins(),
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: '',
+						},
+					},
+					'css-loader',
+				],
+			},
+			{
+				test: /\.less$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					'css-loader',
+					'less-loader',
+				],
+			},
+			{
+				test: /\.s[ac]ss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(png|jpg|svg|gif)$/,
+				use: ['file-loader'],
+			},
+			{
+				test: /\.(ttf|woff|woff2|eot)$/,
+				use: ['file-loader'],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-proposal-class-properties'],
+					},
+				},
+			},
+		],
+	},
+};
